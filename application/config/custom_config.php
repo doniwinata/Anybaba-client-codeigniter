b8fb3dbbbe087150123700aa0b641fe5bf02c228
Doni Winata <doniwinata03@gmail.com>
2015-12-30 14:41:00 +0800

    'wew'

--- application/config/custom_config.php
+++ application/config/custom_config.php
@@ -1,18 +1,21 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 //adding config items.
 use \Firebase\JWT\JWT;
-$url = 'http://localhost:3000';
+$url = 'https://localhost:3000';
+
 $handle = curl_init($url);
 
 
 curl_setopt($handle, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($handle, CURLOPT_TIMEOUT,1);
+curl_setopt ($handle, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt ($handle, CURLOPT_SSL_VERIFYPEER, 0); 
 $result = curl_exec($handle);
 
 $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
 
 if($httpCode == 0){
-	$config['serv_url'] = 'http://localhost:3030';
+	$config['serv_url'] = 'https://localhost:3030';
 }
 else{
 	$config['serv_url'] = $url;
